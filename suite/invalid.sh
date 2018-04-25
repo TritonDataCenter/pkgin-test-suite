f689ead736a6147d15ed5f5b1e89a93a76dbacd8
Jonathan Perkin <jperkin@joyent.com>
2018-04-25 14:15:38 +0100

    Re-enable the invalid tests.

--- suite/invalid.sh
+++ suite/invalid.sh
@@ -1,22 +1,22 @@
 #
-# This repository tests download failures.
+# This repository tests invalid or exaggerated pkg_summary values.
 #
 
 pkg_filesize="badfilesize-1.0"
 pkg_sizepkg="badsizepkg-1.0"
 
-@test "${REPO_NAME} 1" {
-	skip
+@test "${REPO_NAME} test massive FILE_SIZE" {
 	run pkgin -y install ${pkg_filesize}
-	[ ${status} -eq 123 ]
+	[ ${status} -eq 1 ]
+	output_match "does not have enough space for download"
 }
-@test "${REPO_NAME} 2" {
-	skip
+@test "${REPO_NAME} test massive SIZE_PKG" {
 	run pkgin -y install ${pkg_sizepkg}
-	[ ${status} -eq 123 ]
+	[ ${status} -eq 1 ]
+	output_match "does not have enough space for installation"
 }
-@test "${REPO_NAME} 3" {
-	skip
+@test "${REPO_NAME} test pkgin stats with large values" {
 	run pkgin stats
-	[ ${status} -eq 123 ]
+	[ ${status} -eq 0 ]
+	output_match "Total size of packages: 877P"
 }
