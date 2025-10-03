e1080bdf8970805e8c89e34b815409d604ea9cec
Jonathan Perkin <jonathan@perkin.org.uk>
2025-10-03 11:46:46 +0100

    bats: Update to v1.12.0.

--- lib/bats-core/test_functions.bash
+++ lib/bats-core/test_functions.bash
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-BATS_TEST_DIRNAME="${BATS_TEST_FILENAME%/*}"
-BATS_TEST_NAMES=()
+# this must be called for each test file!
+_bats_test_functions_setup() { # <BATS_TEST_NUMBER>
+  BATS_TEST_DIRNAME="${BATS_TEST_FILENAME%/*}"
+  BATS_TEST_NAMES=()
+  # shellcheck disable=SC2034
+  BATS_TEST_NUMBER=${1?}
+}
 
 # shellcheck source=lib/bats-core/warnings.bash
-source "$BATS_ROOT/lib/bats-core/warnings.bash"
+source "$BATS_ROOT/$BATS_LIBDIR/bats-core/warnings.bash"
 
 # find_in_bats_lib_path echoes the first recognized load path to
 # a library in BATS_LIB_PATH or relative to BATS_TEST_DIRNAME.
 #
 # Libraries relative to BATS_TEST_DIRNAME take precedence over
 # BATS_LIB_PATH.
-#
-# Library load paths are recognized using find_library_load_path.
 #
 # If no library is found find_in_bats_lib_path returns 1.
 find_in_bats_lib_path() { # <return-var> <library-name>
@@ -75,18 +78,12 @@ bats_internal_load() {
 # bats_load_safe accepts an argument called 'slug' and attempts to find and
 # source a library based on the slug.
 #
-# A slug can be an absolute path, a library name or a relative path.
+# A slug can be an absolute path or a relative path.
 #
-# If the slug is an absolute path bats_load_safe attempts to find the library
-# load path using find_library_load_path.
-# What is considered a library load path is documented in the
-# documentation for find_library_load_path.
+# If the slug is not an absolute path it is resolved relative to
+# BATS_TEST_DIRNAME
 #
-# If the slug is not an absolute path it is considered a library name or
-# relative path. bats_load_safe attempts to find the library load path using
-# find_in_bats_lib_path.
-#
-# If bats_load_safe can find a library load path it is passed to bats_internal_load.
+# The resolved slug is passed to bats_internal_load.
 # If bats_internal_load fails bats_load_safe returns 1.
 #
 # If no library load path can be found bats_load_safe prints an error message
@@ -163,20 +160,144 @@ bats_merge_stdout_and_stderr() {
 
 # write separate lines from <input-var> into <output-array>
 bats_separate_lines() { # <output-array> <input-var>
-  local output_array_name="$1"
-  local input_var_name="$2"
+  local -r output_array_name="$1"
+  local -r input_var_name="$2"
+  local input="${!input_var_name}"
   if [[ $keep_empty_lines ]]; then
     local bats_separate_lines_lines=()
-    if [[ -n "${!input_var_name}" ]]; then # avoid getting an empty line for empty input
+    if [[ -n "$input" ]]; then # avoid getting an empty line for empty input
+      # remove one trailing \n if it exists to compensate its addition by <<<
+      input=${input%$'\n'}
       while IFS= read -r line; do
         bats_separate_lines_lines+=("$line")
-      done <<<"${!input_var_name}"
+      done <<<"${input}"
     fi
     eval "${output_array_name}=(\"\${bats_separate_lines_lines[@]}\")"
   else
     # shellcheck disable=SC2034,SC2206
     IFS=$'\n' read -d '' -r -a "$output_array_name" <<<"${!input_var_name}" || true # don't fail due to EOF
   fi
+}
+
+bats_pipe() { # [-N] [--] command0 [ \| command1 [ \| command2 [...]]]
+  # This will run each command given, piping them appropriately.
+  # Meant to be used in combination with `run` helper to allow piped commands
+  # to be used.
+  # Note that `\|` must be used, not `|`.
+  # By default, the exit code of this command will be the last failure in the
+  # chain of piped commands (similar to `set -o pipefail`).
+  # Supplying -N (e.g. -0) will instead always use the exit code of the command
+  # at that position in the chain.
+  # --returned-status=N could be used as an alternative to -N. This also allows
+  # for negative values (which count from the end in reverse order).
+
+  local pipestatus_position=
+
+  # parse options starting with -
+  while [[ $# -gt 0 ]] && [[ $1 == -* ]]; do
+    case "$1" in
+    -[0-9]*)
+      pipestatus_position="${1#-}"
+      ;;
+    --returned-status*)
+      if [ "$1" = "--returned-status" ]; then
+        pipestatus_position="$2"
+        shift
+      elif [[ "$1" =~ ^--returned-status= ]]; then
+        pipestatus_position="${1#--returned-status=}"
+      else
+        printf "Usage error: unknown flag '%s'" "$1" >&2
+        return 1
+      fi
+      ;;
+    --)
+      shift # eat the -- before breaking away
+      break
+      ;;
+    *)
+      printf "Usage error: unknown flag '%s'" "$1" >&2
+      return 1
+      ;;
+    esac
+    shift
+  done
+
+  # parse and validate arguments, escape as necessary
+  local -a commands_and_args=("$@")
+  local -a escaped_args=()
+  local -i pipe_count=0
+  local -i previous_pipe_index=-1
+  local -i index=0
+  for (( index = 0; index < $#; index++ )); do
+    local current_command_or_arg="${commands_and_args[$index]}"
+    local escaped_arg="$current_command_or_arg"
+    if [[ "$current_command_or_arg" != '|' ]]; then
+      # escape args to protect them when eval'd (e.g. if they contain whitespace).
+      printf -v escaped_arg "%q" "$current_command_or_arg"
+    elif [ "$current_command_or_arg" = "|" ]; then
+      if [ "$index" -eq 0 ]; then
+        printf "Usage error: Cannot have leading \`\\|\`.\n" >&2
+        return 1
+      fi
+      if (( (previous_pipe_index + 1) >= index )); then
+        printf "Usage error: Cannot have consecutive \`\\|\`. Found at argument position '%s'.\n" "$index" >&2
+        return 1
+      fi
+      (( ++pipe_count ))
+      previous_pipe_index="$index"
+    fi
+    escaped_args+=("$escaped_arg")
+  done
+
+  if (( (previous_pipe_index > 0) && (previous_pipe_index == ($# - 1)) )); then
+    printf "Usage error: Cannot have trailing \`\\|\`.\n" >&2
+    return 1
+  fi
+
+  if (( pipe_count == 0 )); then
+    # Don't allow for no pipes. This might be a typo in the test,
+    # e.g. `run bats_pipe command0 | command1`
+    # instead of `run bats_pipe command0 \| command1`
+    # Unfortunately, we can't catch `run bats_pipe command0 \| command1 | command2`.
+    # But this check is better than just allowing no pipes.
+    printf "Usage error: No \`\\|\`s found. Is this an error?\n" >&2
+    return 1
+  fi
+
+  # there will be pipe_count + 1 entries in PIPE_STATUS (pipe_count number of \|'s between each entry).
+  # valid indices are [-(pipe_count + 1), pipe_count]
+  if [ -n "$pipestatus_position" ] && (( (pipestatus_position > pipe_count) || (-pipestatus_position > (pipe_count + 1)) )); then
+    printf "Usage error: Too large of -N argument (or --returned-status) given. Argument value: '%s'.\n" "$pipestatus_position" >&2
+    return 1
+  fi
+
+  # run commands and return appropriate pipe status
+  local -a __bats_pipe_eval_pipe_status=()
+  eval "${escaped_args[*]}" '; __bats_pipe_eval_pipe_status=(${PIPESTATUS[@]})'
+
+  local result_status=
+  if [ -z "$pipestatus_position" ]; then
+    # if we are performing default "last failure" behavior,
+    # iterate backwards through pipe_status to find the last error.
+    result_status=0
+    for index in "${!__bats_pipe_eval_pipe_status[@]}"; do
+      # OSX bash doesn't support negative indexing.
+      local backward_iter_index="$((${#__bats_pipe_eval_pipe_status[@]} - index - 1))"
+      local status_at_backward_iter_index="${__bats_pipe_eval_pipe_status[$backward_iter_index]}"
+      if (( status_at_backward_iter_index != 0 )); then
+        result_status="$status_at_backward_iter_index"
+        break;
+      fi
+    done
+  elif (( pipestatus_position >= 0 )); then
+    result_status="${__bats_pipe_eval_pipe_status[$pipestatus_position]}"
+  else
+    # Must use positive values for some bash's (like OSX).
+    local backward_iter_index="$((${#__bats_pipe_eval_pipe_status[@]} + pipestatus_position))"
+    result_status="${__bats_pipe_eval_pipe_status[$backward_iter_index]}"
+  fi
+
+  return "$result_status"
 }
 
 run() { # [!|-N] [--keep-empty-lines] [--separate-stderr] [--] <command to run...>
@@ -338,17 +459,44 @@ skip() {
   exit 0
 }
 
-bats_test_begin() {
-  BATS_TEST_DESCRIPTION="$1"
-  if [[ -n "$BATS_EXTENDED_SYNTAX" ]]; then
-    printf 'begin %d %s\n' "$BATS_SUITE_TEST_NUMBER" "${BATS_TEST_NAME_PREFIX:-}$BATS_TEST_DESCRIPTION" >&3
-  fi
-  setup
-}
-
 bats_test_function() {
-  local test_name="$1"
-  BATS_TEST_NAMES+=("$test_name")
+  local tags=()
+  while (( $# > 0 )); do
+    case "$1" in
+      --description)
+        local test_description=
+        # use eval to resolve variable references in test names
+        eval "printf -v test_description '%s' \"$2\""
+        shift 2
+      ;;
+      --tags)
+        IFS=',' read -ra tags <<<"$2"
+        shift 2
+      ;;
+      --)
+        shift
+        break
+      ;;
+      *)
+        printf "ERROR: unknown option %s for bats_test_function" "$1" >&2
+        exit 1
+      ;;
+    esac
+  done
+  BATS_TEST_NAMES+=("$*")
+  local quoted_parameters
+  printf -v quoted_parameters " %q" "$@"
+  quoted_parameters=${quoted_parameters:1} # cut off leading space
+
+  # if this is the currently selected test, set tags and name
+  # this should only be entered from bats-exec-test
+  if [[ ${BATS_TEST_NAME-} == "$quoted_parameters"  ]]; then
+    # shellcheck disable=SC2034
+    BATS_TEST_TAGS=("${tags[@]+${tags[@]}}")
+    export BATS_TEST_DESCRIPTION="${test_description-$*}"
+    # shellcheck disable=SC2034
+    BATS_TEST_COMMAND=("$@")    
+  fi
 }
 
 # decides whether a failed test should be run again
